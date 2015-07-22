48403dc8de8aded85d22544a92055ad6ad5a929a
karlfant <fant@theseusresearch.com>
2015-07-22 15:13:36 -0700

    compact code, init start 0

--- basics/pipeline1.v
+++ basics/pipeline1.v
@@ -8,25 +8,19 @@
 
 module pipeline1;
 
-wire  A;
-wire  B;
-wire  C;
-wire  D;
-wire  E;
+wire  A, B, C, D, E;
 wire ACOMP, BCOMP, CCOMP, DCOMP, ECOMP;
 reg init = 0;
   /* Make an init that pulses once. */
  initial begin
-     # 1 init = 1;
+     # 0 init = 1;
      # 20 init = 0;
      # 1000 $stop;
-
   end
 initial
  begin
     $dumpfile("pipeline1.vcd");
     $dumpvars(0,pipeline1);
-    
  end
 
 ///// Testbench
